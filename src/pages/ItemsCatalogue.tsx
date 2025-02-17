d1bf25e500548a52dd3fdede5124aba950873543
Saswata Das <dassaswata1996@gmail.com>
2025-02-18 04:22:04 +0530

    add filter and sort design

--- src/pages/ItemsCatalogue.tsx
+++ src/pages/ItemsCatalogue.tsx
@@ -2,14 +2,16 @@ import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import { itemsCatalogueSearchProducts } from "../api.services";
 import { ItemCardDef } from "./pages.types";
-import { Masonry } from "@mui/lab";
-import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import { Box, Button, Card, CardContent, CardMedia, FormControl, FormControlLabel, Input, InputAdornment, Radio, RadioGroup, Rating, Skeleton, Typography } from "@mui/material";
 import "./pages.styles.css";
 import Header from "../components/Header.component";
 import { useHomeStore } from "./pages.stores";
+import { Suspense, useMemo, useState } from "react";
+import styles from "./pages.styles";
 
 export default function ItemsCatalogue() {
     const { searchedText, suggestedText } = useParams();
+    const [filterConfig, setFilterConfig] = useState({});
     const { showNavigation, toggleShowNavigation } = useHomeStore();
 
     const productsFromSearchedText = useQuery({
@@ -24,6 +26,24 @@ export default function ItemsCatalogue() {
         enabled: suggestedText && suggestedText.trim().length > 0 ? true : false
     })
 
+    const productsToShow = useMemo(() => {
+        if (productsFromSearchedText.isLoading || productsFromSuggestedText.isLoading) return [];
+        return [...productsFromSuggestedText.data, ...productsFromSearchedText.data];
+    }, [productsFromSearchedText.data, productsFromSuggestedText.data, productsFromSearchedText.isLoading, productsFromSuggestedText.isLoading])
+
+    const ProductCard = ({item}:{item:ItemCardDef}) => {
+        const [imageLoading, setImageLoading] = useState(true);
+        return (
+            <Card sx={styles.itemCatalogueItemCard}>
+                {imageLoading && <Skeleton variant="rectangular" width="100%" height={300} />}
+                <CardMedia component="img" image={item.thumbnail} alt="" onLoad={() => setImageLoading(false)} />
+                <CardContent>
+                    <Typography>{item.title}</Typography>
+                </CardContent>
+            </Card>
+        )
+    }
+
     return (
         <div>
             <Header toggleShowNavigation={toggleShowNavigation} />
@@ -32,29 +52,37 @@ export default function ItemsCatalogue() {
                 <span>Loading...</span> :
                 <div className="itemCatalogueSections">
                     <div className="itemCatalogueFilterAndSort">
-                        <div>Sort</div>
-                        <div>Price</div>
-                        <div>Rating</div>
-                        <div>Availability</div>
+                        <Typography variant="subtitle1" fontWeight={800}>Sort By:</Typography>
+                        <Box sx={styles.filterAndSortColSec}>
+                            <Button variant="text" size="small">Relevance</Button>
+                            <Button variant="text" size="small">Price: Low to High</Button>
+                            <Button variant="text" size="small">Price: High to Low</Button>
+                            <Button variant="text" size="small">Rating: Low to High</Button>
+                            <Button variant="text" size="small">Rating: High to Low</Button>
+                        </Box>
+                        <Typography variant="subtitle1" fontWeight={800}>Price Range:</Typography>
+                        <Box sx={styles.filterAndSortRowSec}>
+                            <Input type="number" placeholder="Min" startAdornment={<InputAdornment position="start">$</InputAdornment>} />
+                            <Input type="number" placeholder="Max" startAdornment={<InputAdornment position="start">$</InputAdornment>} />
+                        </Box>
+                        <Typography variant="subtitle1" fontWeight={800}>Minimum Rating:</Typography>
+                        <Box>
+                            <Rating precision={0.5} defaultValue={0} />
+                        </Box>
+                        <Typography variant="subtitle1" fontWeight={800}>Availability:</Typography>
+                        <Box>
+                            <FormControl>
+                                <RadioGroup defaultValue="In Stock">
+                                    <FormControlLabel value="In Stock" label="In Stock" control={<Radio/>} />
+                                    <FormControlLabel value="Low Stock" label="Low Stock" control={<Radio/>} />
+                                    <FormControlLabel value="Out of Stock" label="Out of Stock" control={<Radio/>} />
+                                </RadioGroup>
+                            </FormControl>
+                        </Box>
                     </div>
-                    <Masonry spacing={2}>
-                        {productsFromSuggestedText.data.map((item:ItemCardDef) => 
-                            <Card>
-                                <CardMedia component="img" image={item.thumbnail} alt="" />
-                                <CardContent>
-                                    <Typography>{item.title}</Typography>
-                                </CardContent>
-                            </Card>
-                        )}
-                        {productsFromSearchedText.data.map((item:ItemCardDef) => 
-                            <Card>
-                                <CardMedia component="img" image={item.thumbnail} alt="" />
-                                <CardContent>
-                                    <Typography>{item.title}</Typography>
-                                </CardContent>
-                            </Card>
-                        )}
-                    </Masonry>
+                    <Box sx={styles.itemCatalogueItemsList}>
+                        {productsToShow.map((item:ItemCardDef) => <ProductCard key={item.id} item={item} />)}
+                    </Box>
                 </div>
             }
         </div>
--- src/pages/pages.styles.ts
+++ src/pages/pages.styles.ts
@@ -1,7 +1,31 @@
 const styles = {
-    itemCatalogueCard: {
-        
-    }
+    itemCatalogueItemsList: {
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        gap: 4,
+
+        width: '100%'
+    },
+    itemCatalogueItemCard: {
+        width: '12vw',
+    },
+    filterAndSortRowSec: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+        gap: '8px'
+    },
+    filterAndSortColSec: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+        gap: '8px'
+    },
 }
 
 export default styles;
